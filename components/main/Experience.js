f9c6cb596bc0736bc2fbd7d5294f86c526f69ec1
genesis <genesis041835@gmail.com>
2020-09-05 16:20:09 +0900

    토이프로젝트 내용 추가

--- components/main/Experience.js
+++ components/main/Experience.js
@@ -10,7 +10,8 @@ export default function Experience() {
         date: '2019~2020',
         link: 'https://app.playauto.io/',
         stack: 'node.js, C#, php, angular',
-        contents: `Playauto 2.0 서비스 쇼핑몰 유지 보수 및 연동 개발
+        descript:'쇼핑몰 통합 솔루션으로 판매자의 주문, 상품, 고객, 출고, 재고관리까지 편리하게 관리하는 서비스',
+        contents: `Playauto 2.0 서비스에서 쇼핑몰 유지 보수 및 연동 개발을 하였으며 연동 개발한 쇼핑몰 리스트로는 오늘의집, 신세계TV쇼핑, W쇼핑, emart mall, 카페24, 티몬 등이 있습니다. 
         `
     }, 
     {
@@ -18,26 +19,29 @@ export default function Experience() {
         date: '2018',
         stack: 'mysql, Angular, node, express.js',
         link: 'http://hgudegree.cafe24app.com/',
-        contents: '한동대 디자인학과 졸업생들의 졸업 과제를 요약해서 보여주는 웹 사이트'
+        descript: '한동대 디자인학과 졸업생들의 졸업 과제를 요약해서 보여주는 웹 사이트',
+        contents: '혼자 개발을 진행하였으며 FE는 Angular로 BE은 Express를 이용하여 개발하였습니다.'
     },
     {
         title: '장덕한방병원 자가진단',
         date: '2016',
         link:  'https://s.jangdeuk.com/include/footTest.php?landing=homepc',
         stack: 'php, mysql',
-        contents: '장덕 한방병원의 자가진단을 통해 고객의 상태를 분석하고 상담 예약 시스템'
+        descript: '장덕 한방병원의 자가진단 상담 예약 시스템',
+        contents: '입력을 받는 Form과 병원에서 제작한 로직에 따른 알고리즘을 만들어 문답을 기반으로 고객의 상태를 알려주며 예약자 관리 페이지를 제작'
     },{
         title: '이츠엠',
         date: '2016~2017',
         stack: 'mysql, Angular, node, express.js',
         link: 'http://point.pohang.go.kr/',
-        contents: `포항포인트 - 포항포인트는 포항시가 제공하는 '포항시 포인트' 브랜드를 사용하는 멤버십서비스`
+        descript: `포항포인트 - 포항포인트는 포항시가 제공하는 '포항시 포인트' 브랜드를 사용하는 멤버십서비스`,
+        contents: 'Node.js(express.js)기반으로 백엔드를 혼자 담당하여 제작하였으며 DB는 Oracle을 이용하였고 EC2와 RDS 환경 구축, 웹과 앱 모두 참여하였으며 Auth2.0 로그인 구현 등을 맡아 진행하였습니다.'
     },
         {
         title: '한동대 총학생회 홈페이지',
         date: '2015',
         stack: 'php, mysql',
-        contents: '총학생회 홈페이지 제작'
+        descript: '총학생회 홈페이지 제작'
     }]
     return (
         <ExperienceFrame>
@@ -87,7 +91,7 @@ const TimelineBar = styled.div`
     background-color: black;
     left: 50%;
     top: 45px;
-    height: 455px
+    height: 570px;
 `
 function TimelineDirection ({item, direction}) {
     return(
@@ -97,14 +101,15 @@ function TimelineDirection ({item, direction}) {
                 <div className='tlItem'>
                     <div className='tlTitle'>
                         {item.title}
-                        {item.link&&<a href={item.link}>
+                        {item.link&&<Link href={item.link}><a>
                             <i className="fa fa-link"></i>           
-                        </a>}
+                        </a></Link>}
                     </div>
                     
                     <div className='tlContents'>{item.date}</div>
-                    <div className='tlContents'>{item.stack}</div>
-                    <div className='tlContents'>{item.contents}</div>
+                    <div className='tlContents'>환경 : {item.stack}</div>
+                    <div className='tlContents'>설명 : {item.descript}</div>
+                    {item.contents &&<div className='tlContents'>내가 한일 : {item.contents}</div>}
                     
                 </div>
                 <div className='tlArrow'/>
@@ -142,6 +147,10 @@ function TimelineDirection ({item, direction}) {
                 }
                 .tlTitle{
                     text-align: center;
+                    
+                }
+                .tlTitle > a {
+                    color: black;
                 }
                 .tlContents {
                     font-weight:200;
@@ -152,11 +161,11 @@ function TimelineDirection ({item, direction}) {
                     ${direction}: 90%;
                     width:3%;
                     top: 30px;
-                    background-color: white;
+                    background-color: transparent;
                     height: 20px;
                     border-top: 20px solid transparent;
                     border-bottom: 20px solid transparent;
-                    border-${direction}:: 10px solid #fff;
+                    border-${direction}: 20px solid #fff;
                 }
             `}
             </style>
--- components/main/toy.js
+++ components/main/toy.js
@@ -6,22 +6,33 @@ import { useState } from "react"
 export default function Toy() {
     const project = [
         {
+            no: 1,
             title: '취업 정보 모음',
-            contents: '',
+            contents: '자체 채용 사이트가 있는 회사들의 취업 정보를 Scrapping해서 보여줍니다.',
+            goal: 'nextjs의 BE 연습과 기본적인 구성',
+            link: '/recruit/myreal'
         },
         {
-            title: '클론코딩 - 마이리얼트립',
-            contents: '',
+            no: 3,
+            title: '자소서 사이트',
+            contents: 'nextjs를 이용하여 자기소개 사이트 제작',
+            goal: 'nextjs를 이용하여 취업에 성공하기',
+            link:''
         },
         {
+            no: 2,
             title: '클론코딩 - 마이리얼트립',
-            contents: '',
+            contents: '마이리얼트립 사이트의 클론 코딩으로 사이트의 정보를 cheerio와 Scrapping을 이용하여 실제와 똑같은 페이지 제작',
+            goal: 'styled-component와 jsx를 이용한 css와 redux와 js-cookie를 이용한 로그인 정보 관리',
+            link: 'http://ec2-54-180-109-168.ap-northeast-2.compute.amazonaws.com:3001/'
         }
     ]
     return (
         <ToyFrame>
-            <ToyTitle>Practice</ToyTitle>
-            <ToyItem/>
+            <ToyTitle>NextJS Toy Project</ToyTitle>
+            {project.map(p =>(
+                <ToyItem key={p.no} item={p}/>
+            ))}
         </ToyFrame> 
     )
 }
@@ -31,24 +42,16 @@ const ToyFrame = styled.div`
 const ToyTitle = styled.h2`
     text-align: center;
 `
-function ToyItem() {
+function ToyItem(props) {
     const [toggle, setToggle] = useState(false);
-    const toyContent = toggle ?
-    {
-        display: 'block',
-    }
-    :
-    {
-        display: 'none',
-    }
     return (
         <div className='toyFrame shadow'>
             <div onClick={() => setToggle(!toggle)} className='toyTitle'>
-                Click
+                {toggle &&<i className="fa fa-arrow-down"></i>}
+                {!toggle &&<i className="fa fa-arrow-right"></i>}
+                {' ' + props.item.title}
             </div>
-            <div className='toyContent'>
-                내용~
-            </div>
+            <ToyContent toggle={toggle} contents={props.item.contents} link={props.item.link} goal={props.item.goal}/>
             <style jsx>
             {`
                 .toyFrame {
@@ -56,15 +59,31 @@ function ToyItem() {
                 }
                 .toyTitle {
                     background-color: ${theme.colors.major};
-                    padding: 10px;
+                    padding: 20px;
                     color: white;
-                }
-                .toyContent {
-                    display: ${toyContent.display};
-
                 }
             `}
             </style>
         </div>
+    )
+}
+function ToyContent({toggle, contents, link, goal}) {
+    return(<div>
+        <div className='toyContent'>
+            <div>설명 : {contents}</div>
+            <div>목적 : {goal}</div>
+            <div>
+                {link && <Link href={link}>구경가기</Link>}
+            </div>
+        </div>
+        <style jsx>
+        {`
+            .toyContent {
+                display: ${toggle ? 'block': 'none'};
+                padding: 20px;
+            }
+        `}
+        </style>
+    </div>
     )
 }
